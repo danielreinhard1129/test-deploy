419169f5d2ebd7ff73278585f10003918a74f2f1
danielreinhard1129 <119338658+danielreinhard1129@users.noreply.github.com>
2024-10-25 15:24:36 +0700

    add projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Jumbotron from "./components/Jumbotron";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <Navbar />
       <Jumbotron />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,91 @@
+const Projects = () => {
+  return (
+    <div className="container mx-auto mt-4">
+      <div className="mb-4 space-y-3 text-center">
+        <h1 className="text-4xl font-bold">Projects</h1>
+        <p className="font-light">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+          accusamus, aliquid unde iure reiciendis dolorem ipsa a dolore amet ex.
+          Animi sit cupiditate velit repellat assumenda eos pariatur quasi
+          ipsam?
+        </p>
+      </div>
+
+      <div className="space-y-10">
+        <div className="grid grid-cols-2 gap-8">
+          <div className="my-auto space-y-6">
+            <div className="w-fit rounded-xl bg-yellow-200 p-1">
+              <p className="text-xs text-yellow-600">Fintech</p>
+            </div>
+            <h2 className="text-3xl font-bold">Work Name Here</h2>
+            <p className="font-extralight">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              repudiandae a harum odio repellat unde reiciendis nulla sed saepe
+              ullam, adipisci velit quam eius eos consectetur quo asperiores
+              dignissimos libero.
+            </p>
+            <button className="boder bg-orange-500 px-8 py-2 text-white">
+              View case study
+            </button>
+          </div>
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1521566652839-697aa473761a?q=80&w=2971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="h-[400px] w-full object-cover"
+            />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-8">
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1521566652839-697aa473761a?q=80&w=2971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="h-[400px] w-full object-cover"
+            />
+          </div>
+          <div className="my-auto space-y-6">
+            <div className="w-fit rounded-xl bg-yellow-200 p-1">
+              <p className="text-xs text-yellow-600">Fintech</p>
+            </div>
+            <h2 className="text-3xl font-bold">Work Name Here</h2>
+            <p className="font-extralight">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              repudiandae a harum odio repellat unde reiciendis nulla sed saepe
+              ullam, adipisci velit quam eius eos consectetur quo asperiores
+              dignissimos libero.
+            </p>
+            <button className="boder bg-orange-500 px-8 py-2 text-white">
+              View case study
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-8">
+          <div className="my-auto space-y-6">
+            <div className="w-fit rounded-xl bg-yellow-200 p-1">
+              <p className="text-xs text-yellow-600">Fintech</p>
+            </div>
+            <h2 className="text-3xl font-bold">Work Name Here</h2>
+            <p className="font-extralight">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              repudiandae a harum odio repellat unde reiciendis nulla sed saepe
+              ullam, adipisci velit quam eius eos consectetur quo asperiores
+              dignissimos libero.
+            </p>
+            <button className="boder bg-orange-500 px-8 py-2 text-white">
+              View case study
+            </button>
+          </div>
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1521566652839-697aa473761a?q=80&w=2971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="h-[400px] w-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
